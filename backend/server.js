0275ead00bf961784b12de3effb84a292191d5c6
Tamim-Tur <tamimturki624@gmail.com>
2026-04-01 14:26:47 +0200

    feat(api): add users-endpoint

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,6 @@ app.get("/api/activities", (req, res) => {
     res.json([]);
 });
 
-app.listen(3000, () => console.log("VitalSync API on :3000")); 
+app.listen(3000, () => console.log("VitalSync API on :3000"));
+
+app.get('/api/users', (req, res) => res.json([]));
